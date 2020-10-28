147d031e7c2aa84da46b245f382ca1b8853000c1
Francesco Venturini <fraventu97@hotmail.it>
2020-10-28 11:28:53 +0100

    Introduzione transaction e proposte

---
+++ Transaction.py
@@ -0,0 +1,27 @@
+import time
+
+class Transaction:
+    def __init__(self, sender, receiver, amount):
+        """
+        Crea una nuova transazione.
+        Parametri:
+        - sender (str) è l'account del mittente
+        - receiver (str) è l'account del destinatario
+        - amount (float) è l'ammontare da trasferire
+        """
+        self.sender = sender
+        self.recevier = receiver
+        self.timestamp = time.time()
+        self.amount = amount
+
+    def validate(self):
+        """
+        Controlla se una transazione è valida, ritorna un booleano
+        """
+
+        if self.amount < 0
+            return False
+
+        return True
+
+    
